7993931328c39f1ca94f6c953b04c43efd608d60
gaurisanthosh02 <gaurisanthosh02@gmail.com>
2025-02-19 17:00:21 +0530

    first commit

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,8 +1,24 @@
+const users = require('../models/usermodel')
+
 // register
-exports.registerController =  (req, res) => {
+exports.registerController = async (req, res) => {
     console.log("Inside register controller");
     console.log(req.body);
-    
+    const {username, email, password} = req.body
+    try{
+        const existinguser =  await users.findOne({email})
+        if(existinguser){
+            res.status(406)
+        }else{
+            const newUser = new users({
+                username, email, password
+            })
+            await newUser.save()
+            res.status(200)
+        }
+    }catch(err){
+        console.log(err);
+    }
     res.status(200).json("Register req recieved")
 }
 
--- database/dbConnection.js
+++ database/dbConnection.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose')
 const connectionstring = process.env.DBCONNECTIONSTRING
 
 mongoose.connect(connectionstring).then(res => {
-    console.log("MongoDB Atlas connected successfully with PFServer");
+    console.log("MongoDB Atlas connected successfully with Test Server");
     
 }).catch(err => {
     console.log("MongoDB Atlas connection failed");
